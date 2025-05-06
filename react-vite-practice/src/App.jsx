449a1c4ba7874277eb0e0fe83f010e93010acbc0
Samarpitjain <149796397+Samarpitjain@users.noreply.github.com>
2025-05-07 02:36:00 +0530

    project01 background color change game

--- react-vite-practice/src/App.jsx
+++ react-vite-practice/src/App.jsx
@@ -1,10 +1,20 @@
-
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("olive");
+
 
   return (
-    <h1>fuck you</h1>
-  )
+    <div className="w-full h-screen" style={{ backgroundColor: color }}>
+      <div className="fixed bottom-12 left-1/2 transform -translate-x-1/2 bg-white p-4 rounded-2xl shadow-lg flex flex-wrap justify-center gap-4">
+        <button className="px-4 py-2 rounded-full text-black shadow-lg flex flex-wrap" style={{backgroundColor:"red"}} onClick={()=>setColor("red")}>red</button>
+        <button className="px-4 py-2 rounded-full text-black shadow-lg flex flex-wrap" style={{backgroundColor:"green"}} onClick={()=>setColor("green")}>green</button>
+        <button className="px-4 py-2 rounded-full text-black shadow-lg flex flex-wrap" style={{backgroundColor:"blue"}} onClick={()=>setColor("blue")}>blue</button>
+        <button className="px-4 py-2 rounded-full text-black shadow-lg flex flex-wrap" style={{backgroundColor:"pink"}} onClick={()=>setColor("pink")}>pink</button>
+
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
